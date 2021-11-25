2866b9bb690f068dcc7436c69d64c19d5045c48a
Lepel1998 <luca-nederhorst@hotmail.nl>
2021-11-25 09:18:41 +0100

    last version

--- sudoku.py
+++ sudoku.py
@@ -29,46 +29,48 @@ class Sudoku:
 
         self._grid[y] = new_row
 
+
     def unplace(self, x: int, y: int) -> None:
         """Remove (unplace) a number at x,y."""
         row = self._grid[y]
         new_row = row[:x] + "0" + row[x + 1:]
         self._grid[y] = new_row
 
+
     def value_at(self, x: int, y: int) -> int:
         """Returns the value at x,y."""
-        value = -1
-
-        for i in range(9):
-            for j in range(9):
-                if i == x and j == y:
-                    row = self._grid[y]
-                    value = int(row[x])
+        row = self._grid[y]
+        value = int(row[x])
 
         return value
 
+  
     def options_at(self, x: int, y: int) -> Iterable[int]:
         """Returns all possible values (options) at x,y."""
-        options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        options_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
         # Remove all values from the row
-        for value in self.row_values(y):
-            if value in options:
-                options.remove(value)
+        row_values = self.row_values(y)
+        for value in row_values:
+            if value in options_set:
+                options_set.remove(value)
 
         # Remove all values from the column
-        for value in self.column_values(x):
-            if value in options:
-                options.remove(value)
+        column_values = self.column_values(x)
+        for value in column_values:
+            if value in options_set:
+                options_set.remove(value)
 
         # Get the index of the block based from x,y
         block_index = (y // 3) * 3 + x // 3
 
         # Remove all values from the block
-        for value in self.block_values(block_index):
-            if value in options:
-                options.remove(value)
-
+        block_values = self.block_values(block_index)
+        for value in block_values:
+            if value in options_set:
+                options_set.remove(value)
+        
+        options = list(options_set)
         return options
 
     def next_empty_index(self) -> tuple[int, int]:
@@ -82,6 +84,7 @@ class Sudoku:
             for x in range(9):
                 if self.value_at(x, y) == 0 and next_x == -1 and next_y == -1:
                     next_x, next_y = x, y
+                    return next_x, next_y
 
         return next_x, next_y
 
@@ -129,20 +132,18 @@ class Sudoku:
         """
         values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        result = True
-
         for i in range(9):
             for value in values:
                 if value not in self.column_values(i):
-                    result = False
+                    return False
 
                 if value not in self.row_values(i):
-                    result = False
+                    return False
 
                 if value not in self.block_values(i):
-                    result = False
+                    return False
 
-        return result
+        return True
 
     def __str__(self) -> str:
         representation = ""
